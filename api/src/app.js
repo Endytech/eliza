e68c9bba5d632f930d7d0dc16cd4edd113d6964f
Dremorian <dremoriantr@gmail.com>
2025-01-16 18:53:08 +0200

    update logs view

--- api/src/app.js
+++ api/src/app.js
@@ -386,14 +386,14 @@ async function LogView1(request, response) {
 
                 // Set headers for streaming response
                 response.setHeader('Transfer-Encoding', 'chunked');
-                response.removeHeader('Content-Length'); // Ensure Content-Length is not set
                 console.log('read...');
                 rl.on('line', (line) => {
-                    // console.log('line', line);
+                    console.log('read line');
                     // console.log('json line', JSON.stringify({ line }));
                     // Send each line as a chunk to the client
                     // response.write(JSON.stringify({ line }));  // Wrapping the line in a JSON object, send as a chunk
-                    response.write(JSON.stringify({ line }));  // Wrapping the line in a JSON object, send as a chunk
+                    response.write(line);  // Wrapping the line in a JSON object, send as a chunk
+                    // response.write(JSON.stringify({ line }));  // Wrapping the line in a JSON object, send as a chunk
                 });
 
                 rl.on('close', () => {
