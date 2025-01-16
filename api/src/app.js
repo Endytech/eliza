59fa7d9bdb6e5b93d4b1667adf81d24026e1662a
Dremorian <dremoriantr@gmail.com>
2025-01-16 21:20:43 +0200

    update logs view

--- api/src/app.js
+++ api/src/app.js
@@ -7,6 +7,8 @@ import { spawn } from 'child_process';
 import common_config from './_config';
 import treeKill from 'tree-kill';
 import readline from 'readline';
+// import reverse from 'reverse-stream';
+import backwardsStream from 'fs-backwards-stream';
 
 const app = express();
 const { port } = common_config;
@@ -27,7 +29,7 @@ app.get('/character/stop',StopCharacter);
 app.get('/character/runlist', RunList);
 // View character log
 app.get('/character/log', LogView);
-app.get('/character/log1', LogView1);
+app.get('/character/log1', LogViewStream);
 // app.get('/character/log2', processLogsAndReportErrors());
 // Create character
 app.post('/character', CreateCharacter);
@@ -362,7 +364,7 @@ async function LogView(request, response) {
         });
     }
 }
-async function LogView1(request, response) {
+async function LogViewStream(request, response) {
     try{
         const { query: { character } } = request;
         if (!character || typeof character !== 'string') throw new Error("Character must be string.");
@@ -371,44 +373,36 @@ async function LogView1(request, response) {
         if (runningProcesses[character]) {
             const logPath = runningProcesses[character].log_file;
             if (fs.existsSync(logPath)) {
-                // const logStream = fs.createReadStream(logPath, { encoding: 'utf8', highWaterMark: 1024 * 1024 }); // 1MB chunks
-                // // Pipe the log stream directly to the response
-                // logStream.pipe(response);
-                // logStream.on('error', (error) => {
-                //     throw new Error(`Error reading log file: ${error.message}`);
-                // });
-
-                const fileStream = fs.createReadStream(logPath, { encoding: 'utf8' });
+                // const fileStream = fs.createReadStream(logPath, { encoding: 'utf8' });
+                //
+                // const reversedStream = fileStream.pipe(reverse());
+                const reversedStream = backwardsStream(logPath);
                 const rl = readline.createInterface({
-                    input: fileStream,
+                    input: reversedStream,
                     crlfDelay: Infinity
                 });
-
                 // Set headers for streaming response
-                // response.setHeader('Transfer-Encoding', 'chunked');
-                console.log('read...');
+                response.setHeader('Content-Type', 'application/json');
+                response.setHeader('Transfer-Encoding', 'chunked');
+                response.write('{"status":true,"view":[');
+                let isFirstChunk = true;
                 rl.on('line', (line) => {
-                    console.log('line', line);
-                    console.log('JSON.stringify({ line }', JSON.stringify({ line }));
-                    // console.log('json line', JSON.stringify({ line }));
-                    // Send each line as a chunk to the client
-                    // response.write(JSON.stringify({ line }));  // Wrapping the line in a JSON object, send as a chunk
-                    response.write(line);  // Wrapping the line in a JSON object, send as a chunk
-                    // response.write(JSON.stringify({ line }));  // Wrapping the line in a JSON object, send as a chunk
+                    if (!isFirstChunk) {
+                        response.write(',');
+                    }
+                    isFirstChunk = false;
+                    response.write(JSON.stringify(line));
                 });
 
                 rl.on('close', () => {
-                    console.log('ended');
-                    // End the response once the file is completely read
+                    response.write(']}');
                     response.end();
                 });
-
                 rl.on('error', (error) => {
                     throw new Error(`Error reading log file: ${error.message}`);
                 });
             }
         } else throw new Error(`Character not found in running processes`);
-        // response.json({ status: true, log: logData, view: logData.split('\n') });
     } catch (error) {
         response.status(400).json({
             status: false,
