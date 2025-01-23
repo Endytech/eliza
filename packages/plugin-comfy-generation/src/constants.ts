01aea5335f93946eb32157c93669579da03185a3
Aleksandr <amesemyta@gmail.com>
2025-01-23 19:13:25 +0200

    Add plugin-comfy-generation

---
+++ packages/plugin-comfy-generation/src/constants.ts
@@ -0,0 +1,4 @@
+export const VIDEO_GENERATION_CONSTANTS = {
+    SD_VIDEO_GEN_API_URL: "https://api.runpod.ai/v2/xdbri7uws2192p/run",
+    SD_VIDEO_GEN_API_KEY: "IMAGE_GEN_API_KEY", // Настройка для ключа API
+};
